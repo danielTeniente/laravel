7e8017bb7cf946611361edfe06c3b83dbae71e5c
Daniel <daniel.diaz.bedoya@udla.edu.ec>
2019-07-16 21:23:11 -0500

    Base pgsql

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
-
+$DATABASE_URL=parse_url('postgres://yhzwljfbyetryh:fc26cab6105e176fcce2c0836fae51096eb8add6fae402eb3d1b36506a714cd2@ec2-23-21-177-102.compute-1.amazonaws.com:5432/dv4bctdktm388
+');
 return [
 
     /*
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +57,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
